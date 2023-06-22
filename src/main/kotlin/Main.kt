b4401580c4f045d04dc53e127cfe901cc8513a4b
Magrelaio <caiocesar2004gfgff@gmail.com>
2023-06-22 17:54:38 -0300

    Continuação dos estudos em Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,11 @@
 Este programa serve apenas para estudos de Kotlin e/ou Java. No momento estou apenas estudando Kotlin, mas
  pretendo migrar para o Java futuramente (Isto para agregar mais na linguagem Kotlin).
  */
+
 fun main() {
     println("Hello World!")
-    var nomeDoBao: String = "Caio"
-    println(nomeDoBao)
+    //val nomeaqui
+    formatacao()
 
     //aqui vai estar uma bagunça porque sou eu aprendendo os valores de variaveis numericas.. Ent peço desculpa kk
     println("Valor Maximo de Double é ${Double.MAX_VALUE},e o minimo é ${Double.MIN_VALUE}")
@@ -17,11 +18,19 @@ fun main() {
 
     //variaveis que não são numericas:
 
-    var Caractere: Char = 'a'
-    var Palavra_ou_Frase: String = "ai James da Salada de Fruta Apelão"
-    var Booleano_ein: Boolean = true
+    val Caractere: Char = 'a'
+    val Palavra_ou_Frase: String = "ai James da Salada de Fruta Apelão"
+    val Booleano_ein: Boolean = true
 
     //podemos usar "unsigned" para retirar o minimo do negativo e trazer para o positivo (No exemplo de byte,
     //tirar do -128 e deixar um total de 250... Por exemplo:
-    var num_broken:UByte = 200u //UByte pra transformar em unsigned e "u" dps de 200 para ajudar o compilador a saber que não é um numero "padrão"
+    val num_broken:UByte = 200u //UByte pra transformar em unsigned e "u" dps de 200 para ajudar o compilador a saber que não é um numero "padrão"
+
+    //teste de integrar uma função na outra
+    val soma = sum(a = 3, b = 2)
+    println("O valor da funcao soma é: $soma")
+}
+
+fun sum(a: Int, b: Int): Int{
+    return (a+b)
 }
---
+++ src/main/kotlin/formatacaoStr.kt
@@ -0,0 +1,12 @@
+/* Eu poderia ter utilizado essa função no proprio arquivo Main.kt? Poderia, sim, claro, porem eu quero
+dificultar o maximo possivel para que eu possa aprender de uma melhor forma a contornar muitos obstaculos
+que podem aparecer kk... Enfim, aqui temos mais uma função basica só para eu poder adicionar ao Main.kt;
+E por algum motivo eu não precisei colocar o "Package pasta.arquivo" e afins, eu colocava, dava erro e deixei
+a IDE (IntelliJ) agir por si só kkkkk.
+*/
+
+fun formatacao() {
+    val nome = "Caio"
+    val sobrenome = "Cesar"
+    println("Olá, $nome $sobrenome")
+}
